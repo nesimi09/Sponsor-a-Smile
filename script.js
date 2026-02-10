65d42da70253a884c73faa5b7e41e87360510c55
Nesimi Gashi <nesimi@Nesimis-Mac-mini.local>
2026-02-10 22:51:59 +0100

    Navbar weiß, Toggle-Farben angepasst, Hero Section weiß+rot

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const links = document.querySelectorAll('.menu li a'); 
+
+
+// Active Link beim Klicken + smooth scroll
+const links = document.querySelectorAll('.menu li a');
 links.forEach(link => {
     link.addEventListener('click', function() {
-        links.forEach(l => l.classList.remove('active')); 
-        this.classList.add('active'); 
+        links.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
     });
 });
+
+// Lang Toggle
+document.querySelectorAll('.lang').forEach(el => {
+    el.addEventListener('click', function() {
+        document.querySelectorAll('.lang').forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
